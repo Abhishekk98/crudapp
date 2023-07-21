992d63ad8c32a0617a36ea43ed970d07aef1622a
Abhishekk98 <abhik.09827@gmail.com>
2023-07-21 19:38:18 +0530

    creating form

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import {MdClose} from "react-icons/md"
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+const [addSection,setAddSection] = useState(false)
+
+
+
+const handleSubmit = (e)=>{
+  e.preventDefault()
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <button className="btn btn-add">Add +</button>
+
+    {
+      addSection && (
+        <div className="addContainer">
+
+        <form onSubmit={handleSubmit}>
+        <div className="close-btn"><MdClose/></div>
+
+          <label htmlFor="name">Name :</label>
+          <input type="name" id="name" name="name" />
+          <label htmlFor="email">Email :</label>
+          <input type="email" id="email" name="email" />
+          <label htmlFor="number">Mobile :</label>
+          <input type="number" id="mobile" name="mobile" />
+          <button className="btn">Submit</button>
+        </form>
+      </div>
+      )
+    }
+
+      
     </div>
+    </>
   );
 }
 
