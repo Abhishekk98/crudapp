f89aa031b889a22daeabdbed2040606142934aba
Abhishekk98 <abhik.09827@gmail.com>
2023-07-22 12:04:45 +0530

    MERN CRUD APP

--- client/src/App.js
+++ client/src/App.js
@@ -1,98 +1,179 @@
-import {MdClose} from "react-icons/md"
-import './App.css';
 import { useEffect, useState } from "react";
-import axios from 'axios'
+import "./App.css";
+// import { MdClose } from "react-icons/md";
+import {MdDelete} from "react-icons/md"
+import {MdEdit} from "react-icons/md"
+import Formtable from "./component/Formtable";
 
+import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080/"
+axios.defaults.baseURL = "http://localhost:8080/";
 
 function App() {
+  const [addSection, setAddSection] = useState(false);
+  const [editSection,setEditSection] = useState(false)
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+  });
+  const [formDataEdit, setFormDataEdit] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+    _id : ""
+  });
 
-const [addSection,setAddSection] = useState(false)
-const [formData,setFormData] = useState({
-  name : "",
-  email : "",
-  mobile : "",
-})
+  const [dataList, setDataList] = useState([]);
 
-const [dataList,setDataList] = useState([])
+  const handleOnChange = (e) => {
+    const { value, name } = e.target;
+    setFormData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
 
-const handleOnChange = (e)=>{
-  const {value,name} = e.target
-   setFormData((preve)=>{
-    return{
-      ...preve,
-      [name] : value
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = await axios.post("/create", formData);
+    console.log(data);
+    if (data.data.success) {
+      setAddSection(false);
+      alert(data.data.message);
+      getFetchData()
+      setFormData({
+        name : "",
+        email : "",
+        mobile : ""
+      })
     }
-   })
-}
+  };
 
+  const getFetchData = async () => {
+    const data = await axios.get("/");
+console.log(data)
+    if (data.data.success) {
+      setDataList(data.data.data);
+    }
+  };
+  
 
-const handleSubmit = async(e)=>{
-  e.preventDefault()
-  const data = await axios.post("/create",formData)
-  console.log(data)
-  if(data.data.success){
-    setAddSection(false)
-    alert(data.data.message)
-  }
-}
-
-const getFetchData = async()=>{
-  const data = await axios.get("/")
-  console.log(data)
-  if(data.data.success){
-    setDataList(data.data.data)
-   
-  }
-}
-
-useEffect(()=>{
-    getFetchData( )
-},[]) 
+  useEffect(()=>{
+    getFetchData()
+  },[])
 console.log(dataList)
+  
+  const handleDelete = async(id)=>{
+    const data = await axios.delete("/delete/"+id);
+    getFetchData()
+    alert(data.data.message)
+   
+    if(data.data.success){
 
-  return (
-    <>
-    <div className="container">
-      <button className="btn btn-add" onClick={()=>setAddSection(true)}>Add +</button>
 
-    {
-      addSection && (
-        <div className="addContainer">
+    }
+  }
 
-        <form onSubmit={handleSubmit}>
-        <div className="close-btn"onClick={()=>setAddSection(false)} ><MdClose/></div>
-
-          <label htmlFor="name">Name :</label>
-          <input type="name" id="name" name="name" onChange={handleOnChange} />
-          <label htmlFor="email">Email :</label>
-          <input type="email" id="email" name="email" onChange={handleOnChange} />
-          <label htmlFor="number">Mobile :</label>
-          <input type="number" id="mobile" name="mobile" onChange={handleOnChange} />
-          <button className="btn">Submit</button>
-        </form>
-      </div>
-      )
+  const handleUpdate = async(e)=>{
+    e.preventDefault()
+    const data = await axios.put("/update",formDataEdit)
+    getFetchData()
+    alert(data.data.message)
+      setEditSection(false)
+    if(data.data.success){
+      
     }
 
-    <div className="tableContainer">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Mobile</th>
-            <th>Action</th>
+
+  }
+
+  const handleEditOnChange = async(e)=>{
+      const {value,name} = e.target
+      setFormDataEdit((preve)=>{
+        return{
+          ...preve,
+          [name] : value
+        }
+      })
+  }
+
+  const handleEdit =(el)=>{
+    setFormDataEdit(el)
+    setEditSection(true)
+  }
+
+return (
+    <>
+      <div className="container">
+        <button className="btn btn-add" onClick={() => setAddSection(true)}>
+          Add +
+        </button>
+
+        {
+        addSection && (
+         <Formtable
+         handleSubmit={handleSubmit}
+         handleOnChange={handleOnChange}
+         handleClose={()=>setAddSection(false)}
+         rest={formData}
+         />
+        )
+        }   
+        {
+          editSection && (
+            <Formtable
+            handleSubmit={handleUpdate}
+            handleOnChange={handleEditOnChange}
+            handleClose={()=>setEditSection(false)}
+            rest={formDataEdit}
+            />
+          )
+        }           
+
+        <div className="tableContainer">
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Mobile</th>
+                  <th>Action</th>
+
+                </tr>
+              </thead>
+              <tbody>
+                
+                {dataList[0] ? (
+                  dataList.map((el)=>{
+                    console.log(el)
+                    return(
+                      <tr>
+                      <td>{el.name}</td>
+                      <td>{el.email}</td>
+                      <td>{el.mobile}</td>
+                      <td>
+                        <button className="btn  btn-edit" onClick={()=>handleEdit(el)}><MdEdit/></button>
+                        <button className="btn btn-delete" onClick={()=>handleDelete(el._id)}><MdDelete/></button>
+                      </td>
+                      </tr>
+
+                    )
+                  }))
+                  :(
+                    <p style={{textAlign : "center"}}>No Data Available</p>
+                  )
+                }
+              </tbody>
+
+            </table>
+
+        </div>
 
 
-          </tr>
-        </thead>
-      </table>
-    </div>
-
-      
-    </div>
+      </div>
     </>
   );
 }
